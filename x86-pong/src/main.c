b4880459cbe54f38eee13af73c2953bbf020aee8
Lucas Wantz da Motta <lucaswantz@hotmail.com>
2020-04-20 22:30:46 -0300

    Ajustes até aula 30/03/2020

--- x86-pong/src/main.c
+++ x86-pong/src/main.c
@@ -1,23 +1,32 @@
 #include <sys/io.h>
 #include <gfx/video.h>
 
+// Define as cores do projeto
+static char color_black = 0x000000; // #000000
+static char color_white = 0xFFFFFF; // #FFFFFF
+static char color_debug = 0x00AA00; // #00AA00
+
 // Define o tamanho do tabuleiro (ex 1024 X 768).
-static int tabuleiro_x = 1024;
-static int tabuleiro_y = 768;
+static int tamanho_tabuleiro_x = 1024;
+static int tamanho_tabuleiro_y = 768;
 
 // Controles
 static int altura_barra = 200;
-static int velocity = 20;
+static int velocidade_controle = 20;
+
 
 static int y1 = 234;
 static int y2 = 234;
 static int dy1 = 0;
 static int dy2 = 0;
 
-int x_bola = 512;
+// Bola
+static int velocidade_bola = 5;
+static int x_bola = 512;
 static int y_bola = 312;
 static int dy_bola = 0;
 static int dx_bola = 0;
+
 
 int i = 0;
 
@@ -25,137 +34,163 @@ int i = 0;
 Risquinhos do p1 começam no 482 e vão DECREMENTANDO de 10 em 10
 Risquinhos do p2 começam em 540 e vão INCOREMENTANDO de 10 em 10 
 */
-int score_p1 = 482;
-int score_p2 = 540;
+static int score_p1 = 482;
+static int score_p2 = 540;
 
 int COM = 0x3f8; /* COM1 */
 
-int velocidade = 5;
 
-void desenha_score_p1() {
 
-  if (score_p1 > 100) {
-    draw_square(score_p1, 15, 5, 25, 0x000000);
-    score_p1 -= 10;
-  }
+void desenha_pontuacao_p1() {
+    if (score_p1 > 100) {
+        draw_square(score_p1, 15, 5, 25, color_black);
+        score_p1 -= 10;
+    }
 }
 
-void desenha_score_p2() {
-
-  if (score_p2 < 924) {
-    draw_square(score_p2, 15, 5, 25, 0x000000);
-    score_p2 += 10;
-  }
+void desenha_pontuacao_p2() {
+    if (score_p2 < 924) {
+        draw_square(score_p2, 15, 5, 25, color_black);
+        score_p2 += 10;
+    }
 }
 
-void desenha_p1() {
-  draw_square(60, y1, 20, altura_barra, 0xFF0000);
-  y1 += dy1;
-  draw_square(60, y1, 20, altura_barra, 0x000000);
-  if (y1 < 0) {
-    y1 = 0;
-  }
-  if (y1 > 568) {
-    y1 = 568;
-  }
+void desenha_player_1() {
+    draw_square(60, y1, 20, altura_barra, color_white);
+    y1 += dy1;
+
+    draw_square(60, y1, 20, altura_barra, color_black);
+
+    if (y1 < 0) {
+        y1 = 0;
+    }
+    
+    if (y1 > 568) {
+        y1 = 568;
+    }
 }
 
-void desenha_p2() {
-  draw_square(tabuleiro_x - 80, y2, 20, altura_barra, 0xFF0000);
-  y2 += dy2;
-  draw_square(tabuleiro_x - 80, y2, 20, altura_barra, 0x000000);
-  if (y2 < 0) {
-    y2 = 0;
-  }
-  if (y2 > 568) {
-    y2 = 568;
-  }
+void desenha_player_2() {
+    draw_square(tamanho_tabuleiro_x - 80, y2, 20, altura_barra, color_white);
+    y2 += dy2;
+
+    draw_square(tamanho_tabuleiro_x - 80, y2, 20, altura_barra, color_black);
+    if (y2 < 0) {
+        y2 = 0;
+    }
+
+    if (y2 > 568) {
+        y2 = 568;
+    }
 }
 
 void desenha_bola() {
-  
-    draw_square(x_bola, y_bola, 30, 30, 0XFF0000); 
+    draw_square(x_bola, y_bola, 30, 30, color_white); 
 
     /* Se a bola saiu do esquadro, ela é projetada no meio novamente */
     //   if(x_bola >= 974 || y_bola <= 50){
     //     x_bola = 512;    y_bola = 312;    dx_bola = 0;    dy_bola = 0;
     //   } 
+    
     if(y_bola<0){
-        y_bola= y_bola*(-1);
+        y_bola = y_bola*(-1);
     }else{
-         y_bola= y_bola;
+        y_bola = y_bola;
     }
-	if(x_bola<0){x_bola= x_bola*(-1);}else{x_bola= x_bola;} 
 
-  if(i == 1){
-              x_bola -= velocidade;
-      y_bola -= velocidade;  
-       } else if(i == 0){
-      x_bola += velocidade;
-      y_bola += velocidade;
-  }
+	if(x_bola<0) {
+        x_bola = x_bola*(-1);
+    } else {
+        x_bola = x_bola;
+    } 
 
-  if(y_bola >= 600){
-     i = 1;
-  }
+    if(i == 1) {
+        x_bola -= velocidade_bola;
+        y_bola -= velocidade_bola;  
+    } else if (i == 0) {
+        x_bola += velocidade_bola;
+        y_bola += velocidade_bola;
+    }
 
-   
-  draw_square(x_bola, y_bola, 30, 30, 0X0000FF);
+    if(y_bola >= 600){
+        i = 1;
+    }
+
+    draw_square(x_bola, y_bola, 30, 30, color_white);
 }
-
 
 int desenha_pontilhado() {
-  for (int i = 1; i < tabuleiro_y; i += 8) {
-    draw_square(tabuleiro_x / 2, i, 2, 2, 0x000000);
-  }
+    for (int i = 1; i < tamanho_tabuleiro_y; i += 8) {
+        draw_square(tamanho_tabuleiro_x / 2, i, 2, 2, color_black);
+    }
 }
 
-/* Vai ser executada a cada 18x por segundo, aproximadamente */
+// Vai ser executada a cada 18x por segundo, aproximadamente
 void isr0(void) {
-    desenha_p1();
-    desenha_p2();
+    desenha_player_1();
+    desenha_player_2();
+
     desenha_pontilhado();
-    desenha_score_p1();
-    desenha_score_p2();
+
+    desenha_pontuacao_p1();
+    desenha_pontuacao_p2();
+
     desenha_bola();
+    
     dx_bola += 1;
     dy_bola += 1;
-
 }
 
+// INTERRUPÇÃO QUE CUIDA DAS TECLAS PRESSIONADAS
 void isr1(void) {
-  char keycode;
-  keycode = inb(0x60);
+    char keycode;
+    keycode = inb(0x60);
 
-  if (keycode & 0x80) { // Verifica se a tecla foi pressionada
-    keycode &= ~0x80; // Devolve o bit mais   
-    if (keycode == 17) { // W 
-      dy1 = 0;
-    }
-    if (keycode == 72) { // Seta para cima
-      dy2 = 0;
-    }
-    if (keycode == 31) { // S
-      dy1 = 0;
-    }
-    if (keycode == 80) { // Seta para baixo
-      dy2 = 0;
-    }
+    // Se uma tecla foi pressionada
+    if (keycode & 0x80) { 
+        // Devolve o bit mais   
+        keycode &= ~0x80; 
 
-  } else {
-    if (keycode == 31) { // S
-      dy1 = velocity;
+        // TECLA W
+        if (keycode == 17) {
+            dy1 = 0;
+        }
+
+        // TECLA S
+        if (keycode == 31) {
+            dy1 = 0;
+        }
+
+        // TECLA UP
+        if (keycode == 72) {
+            dy2 = 0;
+        }
+
+        // TECLA DOWN
+        if (keycode == 80) {
+            dy2 = 0;
+        }
+    } else {
+        // TECLA W
+        if (keycode == 17) {
+            dy1 = -velocidade_controle;
+        }
+
+        // TECLA S
+        if (keycode == 31) {
+            dy1 = velocidade_controle;
+        }
+
+        // TECLA DOWN
+        if (keycode == 80) {
+            dy2 = velocidade_controle;
+        }
+
+        // TECLA UP
+        if (keycode == 72) {
+            dy2 = -velocidade_controle;
+        }
     }
-    if (keycode == 80) { // Seta para baixo
-      dy2 = velocity;
-    }
-    if (keycode == 17) { // W 
-      dy1 = -velocity;
-    }
-    if (keycode == 72) { // Seta para cima
-      dy2 = -velocity;
-    }
-  }
 }
 
 void usart_init(int base_addr) {
@@ -203,14 +238,13 @@ void usart_puts(char *str){
 
 int main(void) {
 
-  // Desenha a tela principal
-   draw_square(0, 0, tabuleiro_x, tabuleiro_y, 0xFF0000);
-   usart_init(COM);
-   is_transmit_empty(COM);
+    // Desenha a tela principal
+    draw_square(0, 0, tamanho_tabuleiro_x, tamanho_tabuleiro_y, color_black);
+    usart_init(COM);
+    is_transmit_empty(COM);
    
-  while (1) {
-     
-  }
+    while (1) {     
+    }
 
-  return 0;
+    return 0;
 }
